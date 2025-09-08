e4a8de2fdb3eee8ab223dee46aca97fd9dd70fcb
Kyler Markland <kyler.markland@yahoo.com>
2025-09-08 16:02:09 -0700

    Not working yet, but added git head file reading to find branch user is on, etc

---
+++ src/head.rs
@@ -0,0 +1,50 @@
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::path::{Path, PathBuf};
+
+pub struct Head {
+    pub ref_path: PathBuf,
+    pub head_hash: String,
+}
+
+impl Head {
+    fn new(ref_path: PathBuf, head_hash: String) -> Self {
+        Self { ref_path, head_hash }
+    }
+
+    pub fn from_file(path: &PathBuf) -> Self {
+        let ref_path = PathBuf::from(path);
+        let file = File::open(path).unwrap();
+        let mut reader = BufReader::new(file);
+        let mut buffer = [0; 1024];
+        let mut bytes: Vec<u8> = Vec::new();
+        loop {
+            let bytes_read = reader.read(&mut buffer).unwrap();
+            if bytes_read == 0 {
+                break;
+            }
+            bytes.extend_from_slice(&buffer[..bytes_read]);
+        }
+        Self::new(ref_path, String::from_utf8_lossy(&bytes).to_string())
+    }
+
+    pub fn from_branch(branch: &str) -> Self {
+        let ref_path = PathBuf::from(format!(".git/refs/heads/{}", branch));
+        Self::from_file(&ref_path)
+    }
+
+    pub fn from_head() -> Self {
+        let file = File::open(Path::new(".git/HEAD")).unwrap();
+        let mut reader = BufReader::new(file);
+        let mut buffer = [0; 1024];
+        let mut bytes: Vec<u8> = Vec::new();
+        loop {
+            let bytes_read = reader.read(&mut buffer).unwrap();
+            if bytes_read == 0 {
+                break;
+            }
+            bytes.extend_from_slice(&buffer[..bytes_read]);
+        }
+        Self::from_file(&PathBuf::from(format!(".git/{}", &String::from_utf8_lossy(&bytes)[5..])))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
-use flate2::read::ZlibDecoder;
-use std::io::{Read};
-use std::fs::File;
-use std::path::Path;
 use std::env;
 use std::process;
-use std::io::{BufReader};
-use bstr::BString;
 
 mod commit;
 mod object_finder;
+mod head;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
+    if args.len() < 2 {
         println!("Usage: {} <hash>", args[0]);
         process::exit(1);
     }
-    let commit = commit::Commit::from_hash(&args[1]);
-    println!("Commit: {}", commit);
+    if args[1] == "log" {
+        if args.len() != 2 {
+            println!("Usage: {} log", args[0]);
+            process::exit(1);
+        }
+        let head = head::Head::from_head();
+        println!("Head: {}", head.ref_path.display());
+        println!("Hash: {}", head.head_hash);
+    }
 }
