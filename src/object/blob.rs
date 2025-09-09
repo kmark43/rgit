fc0c9189a380c6ef5c449d78a0030c4cdce41dba
Kyler Markland <kyler.markland@yahoo.com>
2025-09-09 16:11:05 -0700

    Removed print statements for blob

--- src/object/blob.rs
+++ src/object/blob.rs
@@ -29,17 +29,9 @@ impl Blob {
             }
             bytes.extend_from_slice(&buffer[..bytes_read]);
         }
-        let mut hasher = Sha1::new();
-        hasher.update(&bytes);
-        let result = hasher.finalize();
-        println!("File as String:\n{}", String::from_utf8_lossy(&bytes));
-        
+
         let null_pos = bytes.iter().position(|&x| x == b'\0');
-        println!("Null position: {:?}", null_pos);
-        println!("File as bytes:\n{:?}", &bytes);
         bytes = bytes[null_pos.unwrap() + 1..].to_vec();
-        println!("File length: {}", bytes.len());
-        println!("File hash: {}\nSha1 hash: {}", hash.to_string(), hex::encode(result));
         Self::new(hash.to_string(), bytes)
     }
 }
